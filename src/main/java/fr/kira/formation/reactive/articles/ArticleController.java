cace6b8f9d5dda11013634bf687f387b15811360
Killian Raoux <killian@semifir.com>
2023-03-09 16:50:36 +0100

    feat: ajout de swagger

--- src/main/java/fr/kira/formation/reactive/articles/ArticleController.java
+++ src/main/java/fr/kira/formation/reactive/articles/ArticleController.java
@@ -1,5 +1,8 @@
 package fr.kira.formation.reactive.articles;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -14,11 +17,16 @@ public class ArticleController {
         this.articleService = articleService;
     }
 
+    @Operation( summary = "Retourne la liste des articles sauvergardés dans la base de données" )
     @GetMapping
     public Flux<Article> findAll() {
         return articleService.findAll();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Article trouvé"),
+            @ApiResponse(responseCode = "404", description = "Aucun Article ne porte cet Id")
+    })
     @GetMapping("{id}")
     public Mono<Article> findById(@PathVariable Long id) {
         return articleService.findById(id);
